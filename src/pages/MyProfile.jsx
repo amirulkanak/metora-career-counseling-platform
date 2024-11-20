a6154b9f4695246df6311ed4503f53dafd02b81d
amirulkanak <coder.kanak@gmail.com>
2024-11-20 22:21:15 +0600

    My Profile Page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -8,23 +8,39 @@ const MyProfile = () => {
   const [formOpen, setFromOpen] = useState(false);
   return (
     <section className="max-width-wrapper mt-20 kanak min-h-[60rem] mb-20">
-      <div class="mx-auto group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-eminence-200 to-minsk-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
-        <div class="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500"></div>
-        <div class="z-10  group-hover:-translate-y-10 transition-all duration-500">
-          <span class="text-2xl font-semibold">George Johnson</span>
+      <div className="mx-auto group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-200 via-eminence-200 to-minsk-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
+        <div className="w-28 h-28 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500"></div>
+        <div className="z-10  group-hover:-translate-y-10 transition-all duration-500">
+          <span className="text-2xl font-semibold">George Johnson</span>
           <p>Support Specialist</p>
         </div>
         <button
           onClick={() => setFromOpen(!formOpen)}
-          class="bg-eminence-600 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-eminence-700 shadow-btn">
+          className="bg-eminence-600 px-4 py-1 text-slate-50 rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-eminence-700 shadow-btn">
           Edit Profile
         </button>
       </div>
 
       {/* Update form */}
       {formOpen && (
-        <div class="mt-8 bg-slate-50 w-80 p-6 rounded-2xl">
-          <form class="flex flex-col gap-4">kanak</form>
+        <div class="mt-16 kanak mx-auto bg-slate-50 max-w-80 p-6 rounded-2xl">
+          <form>
+            <InputBox type="text" name="fullName" placeholder="Full name" />
+            <InputBox type="text" name="photoUrl" placeholder="Photo URL" />
+
+            {/* error message */}
+            <span>
+              {/* {error && <div className="text-red-500 mb-1">{error}</div>} */}
+            </span>
+
+            <div className="mb-10">
+              <input
+                type="submit"
+                value="Update Profile"
+                className="w-full cursor-pointer rounded-md border btn px-5 py-3 text-base font-medium bg-minsk-500 text-white transition hover:bg-opacity-90"
+              />
+            </div>
+          </form>
         </div>
       )}
     </section>
@@ -32,3 +48,16 @@ const MyProfile = () => {
 };
 
 export default MyProfile;
+
+const InputBox = ({ type, placeholder, name }) => {
+  return (
+    <div className="mb-6">
+      <input
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        className="w-full rounded-md border border-minsk-200 bg-transparent px-5 py-3 text-base outline-none focus-visible:shadow-none "
+      />
+    </div>
+  );
+};
